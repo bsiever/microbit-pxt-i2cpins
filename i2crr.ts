8f57562257af4b67a7b1084ef56e0cf0fd7fa475
Bill Siever <bsiever@gmail.com>
2021-10-26 22:39:05 -0500

    Updated default values for pins.

--- i2crr.ts
+++ i2crr.ts
@@ -6,6 +6,7 @@
 namespace i2crr {
     //% blockId="setI2CPins" block="set i2c data to %sdaPin and clock to %sclPin|"
     //% shim=i2crr::setI2CPins
+    //% sdaPin.defl=DigitalPin.P1 sclPin.defl=DigitalPin.P2
     export function setI2CPins(sdaPin : DigitalPin,  sclPin : DigitalPin) : void {
     }
 }
