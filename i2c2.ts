e09a17d79656b3f84eff86703008fecd8834f28e
Bill Siever <bsiever@gmail.com>
2021-10-26 21:15:14 -0500

    Updated package

--- i2c2.ts
+++ i2c2.ts
@@ -2,7 +2,7 @@
 //% color=#0000FF 
 //% icon="\uf2c8"
 //% block="DS Temp"
-namespace i2cPins {
+namespace pins {
     //% blockId="setI2CPins" block="set i2c data to %sda and clock to %scl|"
     //% shim=pins::setI2CPins
     export function setI2CPins(sda : DigitalPin,  scl : DigitalPin) : void {
