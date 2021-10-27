400dd25abba58761b6d1173b585db5af136a4214
Bill Siever <bsiever@gmail.com>
2021-10-26 21:19:59 -0500

    More cleanup

--- i2c2.ts
+++ i2c2.ts
@@ -1,7 +1,4 @@
 
-//% color=#0000FF 
-//% icon="\uf2c8"
-//% block="DS Temp"
 namespace pins {
     //% blockId="setI2CPins" block="set i2c data to %sda and clock to %scl|"
     //% shim=pins::setI2CPins
--- test.ts
+++ test.ts
@@ -1,9 +1,8 @@
 serial.writeLine("starting...")
-// serial.writeLine("Press A to show temperature")
-// serial.writeLine("If sensor is connected it should show the temperature")
+serial.writeLine("Press A to configure pins (P1=SDA, P2=SCL) and write a number")
+serial.writeLine("Press B to write a number")
 // serial.writeLine("If sensor is NOT connected it should show -Infinity (for temp) and an error message")
 input.onButtonPressed(Button.A, function () {
-  //  pins.setI2CPins(DigitalPin.P0, DigitalPin.P1)
     pins.setI2CPins(DigitalPin.P1, DigitalPin.P2)
     pins.i2cWriteNumber(
     104,
@@ -36,23 +35,3 @@ input.onButtonPressed(Button.B, function () {
     serial.writeValue("z", pins.i2cReadNumber(104, NumberFormat.UInt8BE, true))
     serial.writeValue("t", pins.i2cReadNumber(104, NumberFormat.UInt8BE, false))
 })
-
-// input.onButtonPressed(Button.A, function () {
-//     serial.writeString("temp = ")
-//     serial.writeNumber(dstemp.celsius(DigitalPin.P0))
-//     serial.writeString("\n")
-// //      basic.showNumber(dstemp.celsius(DigitalPin.P0))
-// })
-
-// dstemp.sensorError(function (errorMessage, errorCode, port) {
-//     serial.writeString("\n");
-//     serial.writeString(errorMessage);
-//     serial.writeString(" code=");
-//     serial.writeNumber(errorCode);
-//     serial.writeString(" port=");
-//     serial.writeNumber(port);
-//     serial.writeString("\n");
-// //    basic.showString("err")
-// //    basic.showNumber(ErrorCode)
-// })
-
