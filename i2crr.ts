947d7be136c3d1e705dddfaad160098dd0745e5b
Bill Siever <bsiever@gmail.com>
2021-10-26 22:22:54 -0500

    Ready

--- i2crr.ts
+++ i2crr.ts
@@ -1,10 +1,11 @@
 
+
 //% color=#0000FF 
 //% icon="\uf074"
 //% block="i2c Reroute"
 namespace i2crr {
-    //% blockId="setI2CPins" block="set i2c data to %sda and clock to %scl|"
+    //% blockId="setI2CPins" block="set i2c data to %sdaPin and clock to %sclPin|"
     //% shim=i2crr::setI2CPins
-    export function setI2CPins(sda : DigitalPin,  scl : DigitalPin) : void {
+    export function setI2CPins(sdaPin : DigitalPin,  sclPin : DigitalPin) : void {
     }
 }
