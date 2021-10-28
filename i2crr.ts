3106546f24a2b28151c17b849dbafbce30642a9d
Bill Siever <bsiever@gmail.com>
2021-10-27 19:52:56 -0500

    Updated to remove sim error.

--- i2crr.ts
+++ i2crr.ts
@@ -8,5 +8,7 @@ namespace i2crr {
     //% shim=i2crr::setI2CPins
     //% sdaPin.defl=DigitalPin.P1 sclPin.defl=DigitalPin.P2
     export function setI2CPins(sdaPin : DigitalPin,  sclPin : DigitalPin) : void {
+        // Per https://github.com/microsoft/pxt-microbit/issues/4292
+        0;
     }
 }
